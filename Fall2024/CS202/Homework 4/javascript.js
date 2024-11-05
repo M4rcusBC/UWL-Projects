f785824bcdc74263971cd037e834d68a21a0f089
Marcus B Clements <mbc.uwl@icloud.com>
2024-11-05 16:21:30 -0600

    Implement oddlyEven function for number-string conversion with custom mapping

--- Fall2024/CS202/Homework 4/javascript.js
+++ Fall2024/CS202/Homework 4/javascript.js
@@ -21,7 +21,45 @@ function eligibility(age, state, filed, dependents) {
 }
 
 function oddlyEven(data) {
-    return 0;
+    const oddEvenMap = {
+        0: "*",
+        1: "#",
+        2: "**",
+        3: "##",
+        4: "***",
+        5: "###",
+        6: "****",
+        7: "####",
+        8: "*****",
+        9: "#####",
+    };
+    const reverseOddEvenMap = {
+        "*": "0",
+        "#": "1",
+        "**": "2",
+        "##": "3",
+        "***": "4",
+        "###": "5",
+        "****": "6",
+        "####": "7",
+        "*****": "8",
+        "#####": "9",
+    };
+    if (typeof data === "number" && data >= 0 && Number.isInteger(data)) {
+        return String(data)
+            .split("")
+            .map((digit) => oddEvenMap[digit])
+            .join(":");
+    } else if (typeof data === "string") {
+        const parts = data.split(":");
+        const decoded = parts.map((part) => reverseOddEvenMap[part]);
+        if (decoded.includes(undefined)) {
+            return undefined;
+        }
+        return parseInt(decoded.join(""), 10);
+    } else {
+        return undefined;
+    }
 }
 
 function allAnagrams(words) {
