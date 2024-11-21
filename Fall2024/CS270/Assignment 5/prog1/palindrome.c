ffebc4ce23213af5133ba9ac5fcd742b246e1d1d
Marcus B Clements <mbc.uwl@icloud.com>
2024-11-21 13:08:26 -0600

    Add Makefiles and initial implementations for palindrome and cipher programs

---
+++ Fall2024/CS270/Assignment 5/prog1/palindrome.c
@@ -0,0 +1,35 @@
+/*
+ * Author: TODO
+ * Date:   TODO
+ * Description: TODO
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+// buffer size
+#define LINELEN 1024
+
+int main(int argc, char **argv) {
+  char *fgets_rtn = NULL;  // variable to capture return code
+  char buffer[ LINELEN ];  // where we are reading the data into
+  int line = 1;
+
+  do {
+    // read data (up to LINELEN chars) into buffer from stdin 
+    fgets_rtn = fgets( buffer, LINELEN, stdin );
+    if( fgets_rtn != NULL ) {
+      // fgets was successful!
+      if( '\n' == buffer[ strlen(buffer)-1 ] ) {
+	printf("Remove newline\n");
+	buffer[ strlen(buffer)-1 ] = '\0';
+      }
+      printf("Echo: \"\%s\" (%d)\n", buffer, line);
+      line++;
+    }
+  } while( fgets_rtn != NULL );
+  
+  return 0;
+}
