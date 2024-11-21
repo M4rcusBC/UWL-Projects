22b7cc033d2e02dbc3a9cc8d6f4cbc64f56556de
Marcus C <97267316+M4rcusBC@users.noreply.github.com>
2024-11-21 19:01:18 +0000

    Enhance UI with new styles and add card submission form functionality

--- Fall2024/CS202/cardboard/js/cardboard.js
+++ Fall2024/CS202/cardboard/js/cardboard.js
@@ -12,24 +12,55 @@ cb.getCards((data, err) => {
 function buildPage() {
     let body = document.getElementsByTagName("body")[0];
     let header = document.createElement("header");
-    header.appendChild(document.createElement("h1")).innerText = "Cardboard";
+    header.appendChild(document.createElement("h3")).innerText = "Card Board";
+    header.appendChild(document.createElement('h5')).innerText = "jdoe@uwlax.edu";
     body.appendChild(header);
-    let p = document.createElement("p");
-    p.innerText = 'Hello! Here is some js-injected text!';
-    body.append(p);
-    let main = document.createElement("main");
-    body.appendChild(main);
+
+    let addCardForm = document.createElement("form");
+    addCardForm.className = "add-card-form";
+    
+    let descInput = document.createElement("input");
+    descInput.type = "text";
+    descInput.name = "desc";
+    descInput.required = true;
+    descInput.placeholder = 'description';
+    addCardForm.appendChild(descInput);
+    
+    let urlInput = document.createElement("input");
+    urlInput.type = "text";
+    urlInput.name = "url";
+    urlInput.placeholder = 'url';
+    urlInput.required = true;
+    addCardForm.appendChild(urlInput);
+
+    let submitButton = document.createElement("button");
+    submitButton.type = "submit";
+    submitButton.onclick = () => {
+        let desc = descInput.value;
+        let url = urlInput.value;
+        cb.addCard(desc, url, (data, err) => {
+            if (err) {
+                console.error(err);
+            } else {
+                if (data) {
+                    cards.push(data);
+                }
+            }
+        });
+    };
+    submitButton.innerText = "Add Card";
+    addCardForm.appendChild(submitButton);
+    body.appendChild(addCardForm);
+
+    let div = document.createElement("div");
+    div.classList.add('card-container');
+
+    body.appendChild(div);
     for (let card of cards) {
         let cardDiv = document.createElement("div");
         cardDiv.classList.add("card");
-        let img = document.createElement("img");
-        img.src = card.url;
-        cardDiv.appendChild(img);
-        let desc = document.createElement("p");
-        desc.innerText = card.desc;
-        cardDiv.appendChild(desc);
         let deleteButton = document.createElement("button");
-        deleteButton.innerText = "Delete";
+        deleteButton.innerText = "x";
         deleteButton.onclick = () => {
             cb.removeCard(card.id, (data, err) => {
                 if (err) {
@@ -42,9 +73,14 @@ function buildPage() {
             });
         };
         cardDiv.appendChild(deleteButton);
-        main.appendChild(cardDiv);
+        let img = document.createElement("img");
+        img.src = card.url;
+        cardDiv.appendChild(img);
+        let desc = document.createElement("p");
+        desc.innerText = card.desc;
+        cardDiv.appendChild(desc);
+        div.appendChild(cardDiv);
     }
-    
 }
 
-document.addEventListener('DOMContentLoaded', buildPage);
+document.addEventListener("DOMContentLoaded", buildPage);
