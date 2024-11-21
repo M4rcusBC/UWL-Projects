c6b75908e5ef6df073779951f8a47ee7f9e47c99
Marcus C <97267316+M4rcusBC@users.noreply.github.com>
2024-11-21 19:11:21 +0000

    Refactor code formatting in palindrome and cipher programs for consistency

--- Fall2024/CS270/Assignment 5/prog2/cipher.c
+++ Fall2024/CS270/Assignment 5/prog2/cipher.c
@@ -12,24 +12,27 @@
 // buffer size
 #define MAX_LINE_LENGTH 1024
 
-int main(int argc, char **argv) {
-  char *fgets_rtn = NULL;  // variable to capture return code
-  char buffer[ MAX_LINE_LENGTH ];  // where we are reading the data into
+int main(int argc, char **argv)
+{
+  char *fgets_rtn = NULL;       // variable to capture return code
+  char buffer[MAX_LINE_LENGTH]; // where we are reading the data into
   int line = 1;
 
-  do {
-    // read data (up to LINELEN chars) into buffer from stdin 
-    fgets_rtn = fgets( buffer, MAX_LINE_LENGTH, stdin );
-    if( fgets_rtn != NULL ) {
+  do
+  {
+    // read data (up to LINELEN chars) into buffer from stdin
+    fgets_rtn = fgets(buffer, MAX_LINE_LENGTH, stdin);
+    if (fgets_rtn != NULL)
+    {
       // fgets was successful!
-      if( '\n' == buffer[ strlen(buffer)-1 ] ) {
-	printf("Remove newline\n");
-	buffer[ strlen(buffer)-1 ] = '\0';
+      if ('\n' == buffer[strlen(buffer) - 1])
+      {
+        buffer[strlen(buffer) - 1] = '\0';
       }
       printf("Echo: \"%s\" (%d)\n", buffer, line);
       line++;
     }
-  } while( fgets_rtn != NULL );
-  
+  } while (fgets_rtn != NULL);
+
   return 0;
 }
