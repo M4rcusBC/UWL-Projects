143dfe43c14a9f2da5d48c48748c947ea8d78778
Marcus B Clements <mbc.uwl@icloud.com>
2024-11-23 16:22:24 -0600

    Add core logic for prog1; split logic into two functions

--- Fall2024/CS270/Assignment 5/prog1/palindrome.c
+++ Fall2024/CS270/Assignment 5/prog1/palindrome.c
@@ -12,27 +12,66 @@
 // buffer size
 #define MAX_LINE_LENGTH 1024
 
+int is_pal(char* string);
+char* remove_punc_to_lower(char* string);
+
+char* remove_punc_to_lower(char* string) {
+  const int len = (int)strlen(string);
+  char* result = (char*)malloc(len + 1);
+
+  int j = 0;
+
+  for (int i = 0; i < len; i++) {
+    if (isalnum(string[i])) {
+      result[j++] = (char)tolower(string[i]);
+    }
+  }
+
+  result[j] = "\0"; // Ensures returned strings will always be null-terminated, even if empty
+  return result;
+}
+
+int is_pal(char* string) {
+  char* alpha_num_string = remove_punc_to_lower(string);
+
+  const int len = (int)strlen(alpha_num_string);
+  for (int i = 0; i < ((len) / 2); i++) {
+    if (alpha_num_string[i] != alpha_num_string[len - i - 1]) {
+      return 0;
+    }
+  }
+  return 1;
+
+}
+
 int main(int argc, char **argv)
 {
   char *fgets_rtn = NULL;       // variable to capture return code
   char buffer[MAX_LINE_LENGTH]; // where we are reading the data into
-  int line = 1;
+  int line = 1, pal_count = 0;
+
+  printf("Output:\n");
 
   do
   {
-    // read data (up to LINELEN chars) into buffer from stdin
+    // read data (up to MAX_LINE_LEN chars) into buffer from stdin
     fgets_rtn = fgets(buffer, MAX_LINE_LENGTH, stdin);
-    if (fgets_rtn != NULL)
-    {
+    if (fgets_rtn != NULL) {
       // fgets was successful!
-      if ('\n' == buffer[strlen(buffer) - 1])
-      {
+      if ('\n' == buffer[strlen(buffer) - 1]) {
         buffer[strlen(buffer) - 1] = '\0';
       }
-      printf("Echo: \"%s\" (%d)\n", buffer, line);
+      if (is_pal(buffer)) {
+        pal_count++;
+        printf("[Palindrome!]: %s\n", buffer);
+      } else {
+        printf("[Regular String]: %s\n", buffer);
+      }
       line++;
     }
   } while (fgets_rtn != NULL);
+
+  printf("Summary:\nTotal Palindromes: %d (out of %d strings)", pal_count, (line - 1));
 
   return 0;
 }
