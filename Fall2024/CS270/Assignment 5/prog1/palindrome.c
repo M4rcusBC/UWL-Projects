6ac011a0a3ad3e6b6b0cdef164b7838a444f96c5
Marcus B Clements <mbc.uwl@icloud.com>
2024-11-24 01:56:40 -0600

    Initial implementation of encryption function for prog2

--- Fall2024/CS270/Assignment 5/prog1/palindrome.c
+++ Fall2024/CS270/Assignment 5/prog1/palindrome.c
@@ -12,68 +12,66 @@
 // buffer size
 #define MAX_LINE_LENGTH 1024
 
-int is_pal(char* string);
-char* remove_punc_to_lower(char* string);
+int is_pal(char *string);
 
-char* remove_punc_to_lower(char* string) {
-  const int len = (int)strlen(string);
-  char* result = (char*)malloc(len + 1);
+char *remove_punc_to_lower(char *string);
 
-  int j = 0;
+char *remove_punc_to_lower(char *string) {
+    const int len = (int) strlen(string);
+    char *result = (char *) malloc(len + 1);
 
-  for (int i = 0; i < len; i++) {
-    if (isalnum(string[i])) {
-      result[j++] = (char)tolower(string[i]);
+    int j = 0;
+
+    for (int i = 0; i < len; i++) {
+        if (isalnum(string[i])) {
+            result[j++] = (char) tolower(string[i]);
+        }
     }
-  }
 
-  result[j] = '\0'; // Ensures returned strings will always be null-terminated, even if empty
-  return result;
+    result[j] = '\0'; // Ensures returned strings will always be null-terminated, even if empty
+    return result;
 }
 
-int is_pal(char* string) {
-  char* alpha_num_string = remove_punc_to_lower(string);
+int is_pal(char *string) {
+    char *alpha_num_string = remove_punc_to_lower(string);
 
-  const int len = (int)strlen(alpha_num_string);
-  for (int i = 0; i < ((len) / 2); i++) {
-    if (alpha_num_string[i] != alpha_num_string[len - i - 1]) {
-      free(alpha_num_string);
-      return 0;
+    const int len = (int) strlen(alpha_num_string);
+    for (int i = 0; i < ((len) / 2); i++) {
+        if (alpha_num_string[i] != alpha_num_string[len - i - 1]) {
+            free(alpha_num_string);
+            return 0;
+        }
     }
-  }
-  free(alpha_num_string);
-  return 1;
-
+    free(alpha_num_string);
+    return 1;
 }
 
-int main(int argc, char **argv)
-{
-  char *fgets_rtn = NULL;       // variable to capture return code
-  char buffer[MAX_LINE_LENGTH]; // where we are reading the data into
-  int line = 1, pal_count = 0;
+int main(int argc, char **argv) {
+    char *fgets_rtn = NULL; // variable to capture return code
+    char buffer[MAX_LINE_LENGTH]; // where we are reading the data into
+    int line = 1, pal_count = 0;
 
-  printf("Output:\n");
+    printf("Output:\n");
 
-  do
-  {
-    // read data (up to MAX_LINE_LEN chars) into buffer from stdin
-    fgets_rtn = fgets(buffer, MAX_LINE_LENGTH, stdin);
-    if (fgets_rtn != NULL) {
-      // fgets was successful!
-      if ('\n' == buffer[strlen(buffer) - 1]) {
-        buffer[strlen(buffer) - 1] = '\0';
-      }
-      if (is_pal(buffer)) {
-        pal_count++;
-        printf("[Palindrome!]: %s\n", buffer);
-      } else {
-        printf("[Regular String]: %s\n", buffer);
-      }
-      line++;
-    }
-  } while (fgets_rtn != NULL);
+    do {
+        // read data (up to MAX_LINE_LEN chars) into buffer from stdin
+        fgets_rtn = fgets(buffer, MAX_LINE_LENGTH, stdin);
+        if (fgets_rtn != NULL) {
+            // fgets was successful!
+            if ('\n' == buffer[strlen(buffer) - 1]) {
+                buffer[strlen(buffer) - 1] = '\0';
+            }
+            if (is_pal(buffer)) {
+                pal_count++;
+                printf("[Palindrome!]: %s\n", buffer);
+            } else {
+                printf("[Regular String]: %s\n", buffer);
+            }
+            line++;
+        }
+    } while (fgets_rtn != NULL);
 
-  printf("Summary:\nTotal Palindromes: %d (out of %d strings)\n", pal_count, (line - 1));
+    printf("Summary:\nTotal Palindromes: %d (out of %d strings)\n", pal_count, (line - 1));
 
-  return 0;
+    return 0;
 }
--- Fall2024/CS270/Assignment 5/prog2/cipher.c
+++ Fall2024/CS270/Assignment 5/prog2/cipher.c
@@ -12,38 +12,53 @@
 // buffer size
 #define MAX_LINE_LENGTH 1024
 
-char* encrypt(char* string);
-char* decrypt(char* string);
+void encrypt(char *string, int offset);
+
+char *decrypt(char *string, int offset);
 
 // ASCII Table: https://www.lookuptables.com/
 
+void encrypt(char *string, int offset) {
 
+    for (int i = 0; i < strlen(string); i++) {
+        if (isalpha(string[i])) {
+            const int ascii_value = int(string[i]);
+            const int dup = offset;
 
-int main(int argc, char **argv)
-{
-  char *fgets_rtn = NULL;       // variable to capture return code
-  char buffer[MAX_LINE_LENGTH]; // where we are reading the data into
-  int line = 1;
-
-  // pre-loop control flow; determine encrypt/decrypt and offset
-
-  do
-  {
-    // read data (up to MAX_LINE_LEN chars) into buffer from stdin
-    fgets_rtn = fgets(buffer, MAX_LINE_LENGTH, stdin);
-    if (fgets_rtn != NULL)
-    {
-      // fgets was successful!
-      if ('\n' == buffer[strlen(buffer) - 1])
-      {
-        buffer[strlen(buffer) - 1] = '\0';
-      }
-
-      //loop control flow
-
-      line++;
+            if (ascii_value + offset > 122) {
+                offset -= (122 - ascii_value);
+                offset = offset % 26;
+                string[i] = toupper(char(96 + offset));
+            } else {
+                string[i] = toupper(char(ascii_value + offset));
+            }
+            offset = dup;
+        }
     }
-  } while (fgets_rtn != NULL);
 
-  return 0;
+}
+
+int main(int argc, char **argv) {
+    char *fgets_rtn = NULL; // variable to capture return code
+    char buffer[MAX_LINE_LENGTH]; // where we are reading the data into
+    int line = 1;
+
+    // pre-loop control flow; determine encrypt/decrypt and offset
+
+    do {
+        // read data (up to MAX_LINE_LEN chars) into buffer from stdin
+        fgets_rtn = fgets(buffer, MAX_LINE_LENGTH, stdin);
+        if (fgets_rtn != NULL) {
+            // fgets was successful!
+            if ('\n' == buffer[strlen(buffer) - 1]) {
+                buffer[strlen(buffer) - 1] = '\0';
+            }
+
+            //loop control flow
+
+            line++;
+        }
+    } while (fgets_rtn != NULL);
+
+    return 0;
 }
