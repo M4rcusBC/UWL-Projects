6d3045911ad51b669c3e4b9d312f2203bf7b5514
Marcus B Clements <mbc.uwl@icloud.com>
2024-12-05 21:41:58 -0600

    continue outline; complete core card adding functionality; TODO: links, css, html refactoring for speed

--- Fall2024/CS202/Homework 6 (ajax)/countries.js
+++ Fall2024/CS202/Homework 6 (ajax)/countries.js
@@ -6,6 +6,85 @@ function toggleLinks() {
     }
 }
 
+function addCountry() {
+    let list = document.getElementById('country-list');
+    let query = list.options[list.selectedIndex].value;
+
+    let countries = document.getElementById('country-container').children;
+    for (let i = 0; i < countries.length; i++) {
+        if (countries[i].id === query) {
+            alert('This country already exists on the page. Please add another country or search for a new set.');
+            return;
+        }
+    }
+
+    let urlString = `https://restcountries.com/v3.1/alpha/${query}`;
+
+    $.ajax(urlString, {
+       method: 'GET',
+         accepts: 'application/json',
+        success: (result) => {
+           // Only one country object will ever be returned by this API call
+            let container = document.getElementById("country-container");
+            let countryDiv = document.createElement("div");
+            result = result[0];
+            console.log(result);
+            let cca3 = result['cca3'];
+
+            countryDiv.id = cca3;
+            countryDiv.className = "country";
+
+            let stats = {
+                "Population": result['population'].toLocaleString(),
+                "Common Name": result['name']['common'],
+                "Country Code": cca3,
+                "Continents": result['continents'],
+                "Region": result['region'],
+                "Subregion": result['subregion'],
+                "Capital": result['capital']
+            };
+
+            let officialName = document.createElement("h2");
+            officialName.innerHTML = result['name']['official'];
+            countryDiv.appendChild(officialName);
+
+            let flag = document.createElement("img");
+            flag.src = result['flags']['svg'];
+            flag.alt = result['flags']['alt'];
+            flag.className = 'flag';
+            countryDiv.appendChild(flag);
+
+            let statsKeys = Object.keys(stats);
+            console.log(statsKeys);
+            let keyList = document.createElement("ul");
+            for (let i = 0; i < statsKeys.length; i++) {
+                let stat = document.createElement("li");
+                stat.innerHTML = statsKeys[i];
+                keyList.appendChild(stat);
+            }
+            countryDiv.appendChild(keyList);
+
+            let statsValues = Object.values(stats);
+            console.log(statsValues);
+            let valueList = document.createElement("ul");
+            for (let i = 0; i < statsValues.length; i++) {
+                let stat = document.createElement("li");
+                stat.innerHTML = statsValues[i].toString();
+                valueList.appendChild(stat);
+            }
+            countryDiv.appendChild(valueList);
+
+            container.appendChild(countryDiv);
+        },
+        error: () => {
+            alert('There was an error adding the country to the page.');
+        }
+    });
+
+}
+
+
+
 function buildPage() {
     // Create and configure body elements
     let body = document.getElementsByTagName("body")[0];
@@ -33,9 +112,9 @@ function buildPage() {
     searchCountryForm.onsubmit = (e) => {
         // Prevent the form from submitting and refreshing the page
         e.preventDefault();
-        let name = nameInput.value;
+        let query = nameInput.value;
 
-        let urlString = `https://restcountries.com/v3.1/name/${name}?fields=name,cca3`;
+        let urlString = `https://restcountries.com/v3.1/name/${query}?fields=name,cca3`;
 
         $.ajax(urlString, {
             method: 'GET',
@@ -52,8 +131,8 @@ function buildPage() {
                     list.appendChild(option);
                 }
             },
-            error: (err) => {
-                alert(err);
+            error: () => {
+                alert('Your search did not return any results.');
             }
         });
     }
@@ -72,6 +151,11 @@ function buildPage() {
 
     addCountryForm.appendChild(countryList);
     addCountryForm.appendChild(addCountryButton);
+    addCountryForm.onsubmit = (e) => {
+        e.preventDefault();
+        // No args required here; addCountry finds selected country from list
+        addCountry();
+    }
 
     let wikiLinksCheckbox = document.createElement("input");
     wikiLinksCheckbox.type = "checkbox";
@@ -96,10 +180,10 @@ function buildPage() {
     countryFormContainer.appendChild(wikiLinksDiv);
 
     let countryContainer = document.createElement("section");
-    countryContainer.className = "country-container";
+    countryContainer.id = "country-container";
 
     let footer = document.createElement("footer");
-    footer.innerHTML = "&copy; The Global Ascension Team";
+    footer.innerHTML = "&copy; Countries Inc.";
 
     body.appendChild(header);
     body.appendChild(countryFormContainer);
