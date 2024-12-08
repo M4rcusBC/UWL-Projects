f5ca4b1b5423401a7175bfecd66143612dec0ee6
Marcus B Clements <mbc.uwl@icloud.com>
2024-12-08 11:57:06 -0600

    Add VSCode configuration files and improve output formatting in list_play.c

--- Fall2024/CS270/Assignment 6/Lists/dl_list/list_play.c
+++ Fall2024/CS270/Assignment 6/Lists/dl_list/list_play.c
@@ -15,7 +15,7 @@ int main(int argc, char** argv){
     printf("--------------------\n");
 
     // Test append
-    printf("\nTesting append:\n");
+    printf("Testing append:\n");
     append(&my_list, 10);
     print_list(&my_list);
     append(&my_list, 20);
@@ -25,7 +25,7 @@ int main(int argc, char** argv){
     printf("--------------------\n");
 
     // Test prepend
-    printf("\nTesting prepend:\n");
+    printf("Testing prepend:\n");
     prepend(&my_list, 5);
     print_list(&my_list);
     prepend(&my_list, 1);
@@ -33,7 +33,7 @@ int main(int argc, char** argv){
     printf("--------------------\n");
 
     // Test insert_at
-    printf("\nTesting insert_at:\n");
+    printf("Testing insert_at:\n");
     insert_at(&my_list, 15, 2);  // Insert 15 at index 2
     print_list(&my_list);
     insert_at(&my_list, 35, my_list.size - 1); // Insert 35 at the end
@@ -43,22 +43,24 @@ int main(int argc, char** argv){
     printf("--------------------\n");
 
     // Test index_of
-    printf("\nTesting index_of:\n");
+    printf("Testing index_of:\n");
     print_list(&my_list);
     printf("Index of 15: %d\n", index_of(&my_list, 15));
     printf("Index of 100 (not in list): %d\n", index_of(&my_list, 100));
     printf("--------------------\n");
 
     // Test delete_from_list
-    printf("\nTesting delete_from_list:\n");
+    printf("Testing delete_from_list:\n");
+    print_list(&my_list);
     delete_from_list(&my_list, 15); // Delete existing value
     print_list(&my_list);
     delete_from_list(&my_list, 100); // Try to delete non-existent value
     print_list(&my_list);
+    delete_from_list(&my_list, 0); // Delete first element
     printf("--------------------\n");
 
     // Test delete_at
-    printf("\nTesting delete_at:\n");
+    printf("Testing delete_at:\n");
     delete_at(&my_list, 0); // Delete first element
     print_list(&my_list);
     delete_at(&my_list, my_list.size - 1); // Delete last element
@@ -68,10 +70,12 @@ int main(int argc, char** argv){
     printf("--------------------\n");
 
     // Test set
-    printf("\nTesting set:\n");
+    printf("Testing set:\n");
     set(&my_list, 1, 99);  // Update value at index 1
     print_list(&my_list);
     set(&my_list, -1, 50); // Out-of-bounds index
+    print_list(&my_list);
+    set(&my_list, 3, 33); // Update value at index 3
     print_list(&my_list);
     set(&my_list, 100, 50); // Out-of-bounds index
     print_list(&my_list);
